feec139cfc1d7ca32964340fc99a792e3b93b02a
LoSv2 <artemdemachev@gmail.com>
2022-10-25 14:46:38 +0300

    created

--- main.py
+++ main.py
@@ -13,17 +13,27 @@ def deg_to_gms(deg, formats='string'):
          return  f'{deg1}° {min}мин {sec}сек '
 def gms_to_deg(deg, min, sec):
     """
-    :param deg:
-    :param min:
-    :param sec:
-    :return:
+    :param deg: градусы
+    :param min: минуты
+    :param sec: секунды
+    :return: выводит время и градусы в гладусы
     """
     deg1 = min / 60 + deg
     return deg1
 def deg_to_rad(deg):
+    """
+
+    :param deg: градусы
+    :return: переводит градусы в радианы
+    """
     rad = deg * 180 * pi
     return rad
 def rad_to_deg(rad):
+    """
+
+    :param rad: радианы
+    :return: переводит радианы в градусы
+    """
     deg = rad / 180 / pi
     return deg
 print(deg_to_gms(36.97))
